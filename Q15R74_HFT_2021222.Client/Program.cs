8e6afaf0608797ba48489c8f933742fb9e97ff63
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-04-14 18:12:37 +0200

    Menu added

--- Q15R74_HFT_2021222.Client/Program.cs
+++ Q15R74_HFT_2021222.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Q15R74_HFT_2021222.Logic;
+﻿using ConsoleTools;
+using Q15R74_HFT_2021222.Logic;
 using Q15R74_HFT_2021222.Models;
 using Q15R74_HFT_2021222.Repository;
 using System;
@@ -6,27 +7,87 @@ using System.Linq;
 
 namespace Q15R74_HFT_2021222.Client
 {
-    class Program
+    internal class Program
     {
+
+        static ClubLogic clubLogic;
+        static ManagerLogic managerLogic;
+        static PlayerLogic playerLogic;
+
+        static void Create(string entity)
+        {
+            Console.WriteLine(entity + " create");
+            Console.ReadLine();
+        }
+        static void List(string entity)
+        {
+            if (entity == "Actor")
+            {
+                var items = playerLogic.ReadAll();
+                Console.WriteLine("Id" + "\t" + "Name" + "\t" + "Position" + "\t" + "Age");
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.PlayerId + "\t" + item.Name + "\t" + item.Positon + "\t" + item.Age);
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            Console.WriteLine(entity + " update");
+            Console.ReadLine();
+        }
+        static void Delete(string entity)
+        {
+            Console.WriteLine(entity + " delete");
+            Console.ReadLine();
+        }
+
+
         static void Main(string[] args)
         {
 
 
             var ctx = new FootballDbContext();
-            var repo = new ClubRepository(ctx);
-            var logic = new ClubLogic(repo);
 
-            var items = logic.ReadAll();
+            var ClubRepo = new ClubRepository(ctx);
+            var ManagerRepo = new ManagerRepository(ctx);
+            var PlayerRepo = new PlayerRepository(ctx);
 
-            Club c = new Club()
-            {
-                Name = "AB"
-            };
+            var ClubLogic = new ClubLogic(ClubRepo);
+            var ManagerLogic = new ManagerLogic(ManagerRepo);
+            var PlayerLogic = new PlayerLogic(PlayerRepo);
 
-            logic.Create(c);
+            var clubSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Club"))
+                .Add("Create", () => Create("Club"))
+                .Add("Delete", () => Delete("Club"))
+                .Add("Update", () => Update("Club"))
+                .Add("Exit", ConsoleMenu.Close);
 
-            ;
-            Console.ReadKey();
+            var managerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Manager"))
+                .Add("Create", () => Create("Manager"))
+                .Add("Delete", () => Delete("Manager"))
+                .Add("Update", () => Update("Manager"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var playerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Player"))
+                .Add("Create", () => Create("Player"))
+                .Add("Delete", () => Delete("Player"))
+                .Add("Update", () => Update("Player"))
+                .Add("Exit", ConsoleMenu.Close);
+
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Players", () => playerSubMenu.Show())
+                .Add("Managers", () => managerSubMenu.Show())
+                .Add("Clubs", () => clubSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+
         }
     }
 }
