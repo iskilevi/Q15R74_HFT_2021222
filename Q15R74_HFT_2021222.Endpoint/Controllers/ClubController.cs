83f677f9966a89dd66c3654306abbf3c53c60cf1
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-04-19 19:38:24 +0200

    All controllers added to Endpoint layer

---
+++ Q15R74_HFT_2021222.Endpoint/Controllers/ClubController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Q15R74_HFT_2021222.Logic;
+using Q15R74_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Q15R74_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClubController : ControllerBase
+    {
+
+        IClubLogic logic;
+
+        public ClubController(IClubLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Club> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Club Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Club value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Club value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ Q15R74_HFT_2021222.Endpoint/Controllers/ManagerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Q15R74_HFT_2021222.Logic;
+using Q15R74_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Q15R74_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+
+        IManagerLogic logic;
+
+        public ManagerController(IManagerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Manager> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Manager Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Manager value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Manager value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
