9cdc584c7c7161177b9e7d176a26e1239e82fbb3
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-04-22 16:21:16 +0200

    Create test methods added

--- Q15R74_HFT_2021222.Test/TesterClass.cs
+++ Q15R74_HFT_2021222.Test/TesterClass.cs
@@ -14,21 +14,81 @@ namespace Q15R74_HFT_2021222.Test
     public class TesterClass
     {
 
-        PlayerLogic logic;
+        PlayerLogic pLogic;
         Mock<IRepository<Player>> mockPlayerRepo;
+
+        ClubLogic cLogic;
+        Mock<IRepository<Club>> mockClubRepo;
+
+        //ManagerLogic mLogic;
+        //Mock<IRepository<Manager>> mockManagerRepo;
 
 
         [SetUp]
         public void Init()
         {
             mockPlayerRepo = new Mock<IRepository<Player>>();
-            mockPlayerRepo.Setup(m => m.ReadAll()).Returns(new List<Player>()
-            {
-                new Player(){PlayerId = 1, Name = "Robi"},
-                new Player(){PlayerId = 2, Name = "Árpád"}
+            mockClubRepo = new Mock<IRepository<Club>>();
 
-            }.AsQueryable());
-            logic = new PlayerLogic(mockPlayerRepo.Object);
+
+            pLogic = new PlayerLogic(mockPlayerRepo.Object);
+            cLogic = new ClubLogic(mockClubRepo.Object);
+
+
+            //mockPlayerRepo.Setup(m => m.ReadAll()).Returns(new List<Player>()
+            //{
+            //    new Player(){PlayerId = 1, Name = "Robi"},
+            //    new Player(){PlayerId = 2, Name = "Árpád"}
+
+            //}.AsQueryable());
+        }
+
+        [Test]
+        public void CreatePlayerTestWithCorrectName()
+        {
+            var player = new Player() { Name = "Alberto" };
+
+            //ACT
+            pLogic.Create(player);
+
+            //ASSERT
+            mockPlayerRepo.Verify(r => r.Create(player), Times.Once);
+        }
+
+        [Test]
+        public void CreatePlayerTestWithInCorrectName()
+        {
+            var player = new Player() { Name = "22" };
+            try
+            {
+                //ACT
+                pLogic.Create(player);
+            }
+            catch
+            {
+
+            }
+
+            //ASSERT
+            mockPlayerRepo.Verify(r => r.Create(player), Times.Never);
+        }
+
+        [Test]
+        public void CreateClubTestWithInCorrectName()
+        {
+            var club = new Club() { Name = "a" };
+            try
+            {
+                //ACT
+                cLogic.Create(club);
+            }
+            catch
+            {
+
+            }
+
+            //ASSERT
+            mockClubRepo.Verify(r => r.Create(club), Times.Never);
         }
     }
 }
