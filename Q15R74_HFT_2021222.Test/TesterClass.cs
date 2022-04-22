82a0d7a5506171d810a3f5296ac123a1a8274495
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-04-22 16:04:58 +0200

    Tester class added

---
+++ Q15R74_HFT_2021222.Test/TesterClass.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using Q15R74_HFT_2021222.Logic;
+using Q15R74_HFT_2021222.Models;
+using Q15R74_HFT_2021222.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Q15R74_HFT_2021222.Test
+{
+
+    [TestFixture]
+    public class TesterClass
+    {
+
+        PlayerLogic logic;
+        Mock<IRepository<Player>> mockPlayerRepo;
+
+
+        [SetUp]
+        public void Init()
+        {
+            mockPlayerRepo = new Mock<IRepository<Player>>();
+            mockPlayerRepo.Setup(m => m.ReadAll()).Returns(new List<Player>()
+            {
+                new Player(){PlayerId = 1, Name = "Robi"},
+                new Player(){PlayerId = 2, Name = "Árpád"}
+
+            }.AsQueryable());
+            logic = new PlayerLogic(mockPlayerRepo.Object);
+        }
+    }
+}
