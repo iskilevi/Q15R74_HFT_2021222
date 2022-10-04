d3cd688aa687305d1db16763f969317e86f020d8
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-10-04 10:10:15 +0200

    Basic player commands created

--- FootballDb.WpfClient/MainWindowViewModel.cs
+++ FootballDb.WpfClient/MainWindowViewModel.cs
@@ -1,20 +1,93 @@
-﻿using Q15R74_HFT_2021222.Models;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Q15R74_HFT_2021222.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace FootballDb.WpfClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
 
         public RestCollection<Player> Players { get; set; }
 
+        private Player selectedPlayer;
+
+        public Player SelectedPlayer
+        {
+            get { return selectedPlayer; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedPlayer = new Player()
+                    {
+                        Name = value.Name,
+                        PlayerId = value.PlayerId
+                    };
+                    OnPropertyChanged();
+                    (DeletePlayerCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+
+        public ICommand CreatePlayerCommand { get; set; }
+        public ICommand DeletePlayerCommand { get; set; }
+        public ICommand UpdatePlayerCommand { get; set; }
+
+        public static bool isInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue; 
+            }
+        }
+
         public MainWindowViewModel()
         {
-            Players = new RestCollection<Player>("http://localhost:53910/", "player");
+            if (!isInDesignMode)
+            {
+                Players = new RestCollection<Player>("http://localhost:53910/", "player");
+
+                CreatePlayerCommand = new RelayCommand(() =>
+                {
+                    Players.Add(new Player()
+                    {
+                        Name = "Józsi"
+                    });
+
+                });
+
+                DeletePlayerCommand = new RelayCommand(() =>
+                {
+                    Players.Delete(SelectedPlayer.PlayerId);
+                },
+                () =>
+                {
+                    return SelectedPlayer != null;
+                });
+
+                UpdatePlayerCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Players.Update(SelectedPlayer);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        //ErrorMessage = ex.Message;
+                    }
+
+                });
+            }
         }
     }
 }
