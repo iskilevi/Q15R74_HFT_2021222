d5a4bd30c823073e753f2867a46cf5891c7a819c
iskilevi <leventeiski2@stud.uni-obuda.hu>
2022-10-04 08:28:44 +0200

    MainWindowViewModel Class added

---
+++ FootballDb.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Q15R74_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballDb.WpfClient
+{
+    public class MainWindowViewModel
+    {
+
+        public RestCollection<Player> Players { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Players = new RestCollection<Player>("http://localhost:53910/", "player");
+        }
+    }
+}
